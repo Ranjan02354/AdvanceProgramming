cd5f8f6833f53f4c5b1dc669b30572e982f542a5
Ranjan Malakar <ranjanmalakar731@gmail.com>
2026-06-11 00:08:49 +0530

    Organized all assignments into separate folders

--- Assignment6/Assignment6.java
+++ Assignment6/Assignment6.java
@@ -1,5 +1,11 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 class Student {
 
